e171a27ba1bc520e2d62d2fd65e00aafb5e43282
Nick Yee <njsyee@gmail.com>
2016-01-12 01:09:24 +0000

    Added menu.

--- contact_list.rb
+++ contact_list.rb
@@ -1,8 +1,18 @@
+require 'pry'
 require_relative 'contact'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+ 
+end
 
+
+if ARGV == []
+  puts "Here is a list of available commands:", \
+          "\tnew\t- Create a new contact", \
+          "\tlist\t- List all contacts", \
+          "\tshow\t- Show a contact", \
+          "\tsearch\t- Search contacts"
 end
